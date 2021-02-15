8c4430c7e54a6b8142502f2407d6808bfbbd774e
LoneWolfStarStars <60833356+LoneWolfStarStars@users.noreply.github.com>
2021-02-15 13:30:10 +0800

    二分查找的简单实现
    Simple implementation of binary search.二分查找的简单实现。

---
+++ BinarySearch/BinarySearch—二分查找.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def find(Data,target):
+    low=0
+    high=len(Data)-1
+
+    while low<=high:
+        mid=int((low+high)/2.0)
+        if target>Data[mid]:
+            low=mid+1
+        if target<Data[mid]:
+            high=mid-1
+        if target==Data[mid]:
+            return mid
+    return None
+
+
+# In[4]:
+
+
+#测试函数
+testData1=[1,2,3,4,5,6,7,8,9,10]
+testData2=[33,35,78,93,102,159,253,673,792]
+
+print(find(testData1,10))
+print(find(testData2,792))
+
+
+# In[ ]:
+
+
+
+
